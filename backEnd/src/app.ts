521b2a00839801f1d280877851e8d805d85dd854
Marlon Couto <marloncouto199@gmail.com>
2023-06-09 20:15:50 -0300

    feat: adiciona configuração do Sequelize

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -3,8 +3,8 @@ import express, { Request, Response, Express, json } from "express";
 const app: Express = express();
 app.use(json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello World" });
 });
 
 export default app;
---
+++ backEnd/src/database/config/database.ts
@@ -0,0 +1,13 @@
+import { Options } from 'sequelize';
+
+// configura o banco de dados
+const config: Options = {
+  username: process.env.DB_USER ?? 'root',
+  password: process.env.DB_PASSWORD ?? 'password',
+  database: process.env.DB_NAME ?? 'Trybedex',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT) ?? 3306,
+  dialect: 'mysql',
+};
+
+export = config;
