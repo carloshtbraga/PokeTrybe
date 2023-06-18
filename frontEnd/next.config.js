c1265f706cf3e9d23a8cf98c9932ced1b9c987f0
Marlon Couto <marloncouto199@gmail.com>
2023-06-18 18:49:42 -0300

    fix: renomeia frontend

--- frontEnd/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["ca.slack-edge.com", "media.licdn.com"],
-  }
-}
-
-module.exports = nextConfig
