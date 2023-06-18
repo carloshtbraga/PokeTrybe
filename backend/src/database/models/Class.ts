1121fb95af1b2dbca54e9233bd95f11b16e84abe
Marlon Couto <marloncouto199@gmail.com>
2023-06-18 18:46:54 -0300

    feat: adiciona models

---
+++ backend/src/database/models/Class.ts
@@ -0,0 +1,33 @@
+import {
+    DataTypes,
+    Model,
+    type InferAttributes,
+    type InferCreationAttributes,
+    type CreationOptional
+} from 'sequelize';
+import db from '.';
+
+class Class extends Model<InferAttributes<Class>, InferCreationAttributes<Class>> {
+    declare id: CreationOptional<number>;
+    declare number: number;
+}
+
+Class.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    number: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    }
+}, {
+    sequelize: db,
+    modelName: 'classes',
+    timestamps: true,
+    underscored: true
+});
+
+export default Class;
---
+++ backend/src/database/models/Skill.ts
@@ -0,0 +1,33 @@
+import {
+    DataTypes,
+    Model,
+    type InferAttributes,
+    type InferCreationAttributes,
+    type CreationOptional
+} from 'sequelize';
+import db from '.';
+
+class Skill extends Model<InferAttributes<Skill>, InferCreationAttributes<Skill>> {
+    declare id: CreationOptional<number>;
+    declare name: string;
+}
+
+Skill.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize: db,
+    modelName: 'skills',
+    timestamps: true,
+    underscored: true
+});
+
+export default Skill;
