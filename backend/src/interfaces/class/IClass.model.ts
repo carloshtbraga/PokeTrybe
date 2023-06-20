51ea2445cb2e26cf827bf81abbcf3e27e8d1cb4b
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-20 18:21:11 -0300

    Finaliza: interfaces

--- backend/src/interfaces/class/IClass.model.ts
+++ backend/src/interfaces/class/IClass.model.ts
@@ -4,4 +4,6 @@ export interface IClassModel {
     getAll: () => Promise<IClass[]>;
     getOne: (id: string) => Promise<IClass>;
     create: (person: IClass) => Promise<IClass>;
+    update?: (person: IClass) => Promise<string>;
+    delete?: (id: string) => Promise<string>;
 }
--- backend/src/interfaces/skill/ISkill.model.ts
+++ backend/src/interfaces/skill/ISkill.model.ts
@@ -4,4 +4,6 @@ export interface ISkillModel {
     getAll: () => Promise<ISkill[]>;
     getOne: (id: string) => Promise<ISkill>;
     create: (skill: ISkill) => Promise<ISkill>;
+    update?: (skill: ISkill) => Promise<string>;
+    delete?: (id: string) => Promise<string>;
 }
