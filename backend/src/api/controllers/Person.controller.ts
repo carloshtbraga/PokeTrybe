6fd6d4a5ebd61702ae087a108c5a57ecd8f17d30
joseg-alvesg <cinhoalves85@gmail.com>
2023-08-07 19:09:27 -0300

    implements: Person controller

--- backend/src/api/controllers/Person.controller.ts
+++ backend/src/api/controllers/Person.controller.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response } from 'express';
 import PersonService from '../services/Person.service';
+import { log } from 'console';
 
 export default class PersonController {
     constructor(
@@ -11,6 +12,17 @@ export default class PersonController {
         this._personService.getAll()
             .then((data) => {
                 res.status(200).json(data.data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    }
+
+    public insert(req: Request, res: Response): void {
+        log(req.body);
+        this._personService.insert(req.body)
+            .then((data) => {
+                res.status(201).json(data.data);
             })
             .catch((err) => {
                 res.status(500).json(err);
